bee3bcaddce56cf78ce47197ff9d47b2cad16a24
Leah Barnes <johnson.leahe@gmail.com>
2020-03-19 21:30:28 -0400

    Got reactstrap working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './App.css';
-// import Navbar from './components/navbar/navbar';
+import Navbar from './components/navbar/navbar';
 // import { BrowserRouter as Router } from "react-router-dom";
 import { Button } from 'reactstrap';
 
 
 function App() {
   return (
+    <>
     <div className="App">
-        {/* <Navbar /> */}
+        <Navbar />
         <Button color="danger">Danger!</Button>
     </div>
+    </>
   );
 }
 
