0be8e552e574bb1bf5a1983c7488e8070d1ba5db
Leah Barnes <johnson.leahe@gmail.com>
2020-03-19 21:48:34 -0400

    initial navbar set up with toggler

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/navbar/navbar';
+import Llamabar from './components/navbar/navbar';
 // import { BrowserRouter as Router } from "react-router-dom";
-import { Button } from 'reactstrap';
+
 
 
 function App() {
   return (
     <>
     <div className="App">
-        <Navbar />
-        <Button color="danger">Danger!</Button>
+        <Llamabar/>
+        
     </div>
     </>
   );
