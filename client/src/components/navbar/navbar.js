e009380d86ba0992e9c65fa3de9c737f88476eae
Debbie S <dreategu@gmail.com>
2020-03-21 14:32:24 -0400

    restructured folders

---
+++ client/src/components/navbar/navbar.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import {
+  Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink
+} from 'reactstrap';
+import "./style.css";
+
+
+const Llamabar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      
+      <Navbar className='row' color="light" light expand="md">
+          <div className="col-md-1"></div>
+          <div className="col-md-3">
+            <NavbarBrand href="/">AdventurousLlama</NavbarBrand>
+          </div>
+          <div className="col-md-4"></div>
+          <div className="col-md-4 align-content-end">
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+              <Nav className="mr-auto" navbar>
+                <NavItem>
+                  <NavLink href="#">About</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="#">Profile</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="#">Find Your Adventure</NavLink>
+                </NavItem>
+
+
+
+              </Nav>
+
+            </Collapse>
+          </div>
+        
+
+      </Navbar>
+    </div>
+  );
+}
+
+
+
+export default Llamabar;
