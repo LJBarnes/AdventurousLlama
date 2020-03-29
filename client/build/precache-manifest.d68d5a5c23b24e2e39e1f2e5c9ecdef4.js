4c6d0c7f0e6581d3d3bedf3c9e2546729f159ed7
Debbie S <dreategu@gmail.com>
2020-03-29 01:43:36 -0400

    pushing to heroku

---
+++ client/build/precache-manifest.d68d5a5c23b24e2e39e1f2e5c9ecdef4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76434bad11c73224d417cdfe3519277c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab8a0a0a5e434edfa687",
+    "url": "/static/css/2.0161871a.chunk.css"
+  },
+  {
+    "revision": "d58ceef54c62bca98d67",
+    "url": "/static/css/main.f429842e.chunk.css"
+  },
+  {
+    "revision": "ab8a0a0a5e434edfa687",
+    "url": "/static/js/2.4c57bb9f.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.4c57bb9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d58ceef54c62bca98d67",
+    "url": "/static/js/main.c2d8b3ff.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
